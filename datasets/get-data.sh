3f08d8580ff735faa8e45b75b193a8f6f4761893
lemonsien <lemonsien@gmail.com>
2022-07-29 10:20:24 +0800

    remove big dataset to fit the github size constraint

---
+++ datasets/get-data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# url from https://www.cc.gatech.edu/dimacs10/archive/coauthor.shtml
+
+wget https://www.cc.gatech.edu/dimacs10/archive/data/coauthor/coPapersCiteseer.graph.bz2
+wget https://www.cc.gatech.edu/dimacs10/archive/data/coauthor/coPapersDBLP.graph.bz2
